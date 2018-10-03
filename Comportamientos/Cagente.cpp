3567980cdbe5054e99b4460931ab064355c57003
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-10-03 12:47:04 -0500

    Declaraciones de clases y funciones

--- Comportamientos/Cagente.cpp
+++ Comportamientos/Cagente.cpp
@@ -2,11 +2,61 @@
 
 
 
-Cagente::Cagente()
+cBoid::cBoid()
 {
 }
 
 
-Cagente::~Cagente()
+cBoid::~cBoid()
 {
+}
+
+cVector2 cBoid::GetDir()
+{
+	return cVector2();
+}
+
+cVector2 cBoid::SetDir(cVector2 dir)
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Seek(cVector2 pos, float mag)
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Flee(cVector2 pos, float mag)
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Parsue(cVector2, float mag, float tiem)
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Evade(cVector2, float mag, float tiem)
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Wonder_Ramdom()
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Wonder_dir()
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Follow_path()
+{
+	return cVector2();
+}
+
+cVector2 cBoid::Patrol()
+{
+	return cVector2();
 }
--- Comportamientos/Cagente.h
+++ Comportamientos/Cagente.h
@@ -1,8 +1,21 @@
 #pragma once
-class Cagente
+#include "cObjetoDeJuego.h"
+class cBoid:cGameObject
 {
+private:
+	cVector2 Dir;
 public:
-	Cagente();
-	~Cagente();
+	cVector2 GetDir();
+	cVector2 SetDir(cVector2 dir);
+	cVector2 Seek(cVector2 pos, float mag);
+	cVector2 Flee(cVector2 pos, float mag);
+	cVector2 Parsue(cVector2,float mag, float tiem);
+	cVector2 Evade(cVector2, float mag, float tiem);
+	cVector2 Wonder_Ramdom();
+	cVector2 Wonder_dir();
+	cVector2 Follow_path();
+	cVector2 Patrol();
+	cBoid();
+	~cBoid();
 };
 
